adc573f74b83c50302596ae64bf4fb10fb57829c
Aleksandr Nesterov <aleksandr_nesterov_99@mail.ru>
2020-02-24 13:54:36 +0300

    do some copy

--- src/first/Solution.java
+++ src/first/Solution.java
@@ -11,53 +11,135 @@ public class Solution {
     }
 
     public static void dividingByHalf(double a, double b, double beta, double eps) {
+        int i = 0;
+
         while (Math.abs(b - a) >= eps) {
-            double left = (b + a - beta) / 2;
-            double right = (b + a + beta) / 2;
+            i++;
+            double u1 = (b + a - beta) / 2;
+            double u2 = (b + a + beta) / 2;
 
-            double functionLeft = function(left);
-            double functionRight = function(right);
+            double functionU1 = function(u1);
+            double functionU2 = function(u2);
 
-            if (functionLeft == functionRight) {
-                a = left;
-                b = right;
-            } else if (functionLeft > functionRight) {
-                a = left;
+            if (functionU1 == functionU2) {
+                a = u1;
+                b = u2;
+            } else if (functionU1 > functionU2) {
+                a = u1;
             } else {
-                b = right;
+                b = u2;
             }
         }
 
         System.out.println("Dividing by half: ");
         System.out.println("Point: " + ((b + a) / 2) + " Function: " + function((b + a) / 2));
+        System.out.println("Number of iterations: " + i);
     }
 
     public static void goldenSection(double a, double b, double eps) {
+        int i = 0;
+
+        double alpha = (Math.sqrt(5) - 1) / 2;
+        double alphaOne = (3 - Math.sqrt(5)) / 2;
+
+        double u1 = a + alphaOne * (b - a);
+        double u2 = a + alpha * (b - a);
+
+        double functionU1 = function(u1);
+        double functionU2 = function(u2);
+
         while (Math.abs(b - a) >= eps) {
-            double alphaOne = (Math.sqrt(5) - 1) / 2;
-            double alphaTwo = (3 - Math.sqrt(5)) / 2;
+            i++;
 
-            double left = a + alphaTwo * (b - a);
-            double right = a + alphaOne * (b - a);
-
-            double functionLeft = function(left);
-            double functionRight = function(right);
-
-            if (functionLeft == functionRight) {
-                b = right;
-                a = left;
-            } else if (functionLeft > functionRight) {
-                a = left;
+            if (functionU1 == functionU2) {
+                b = u2;
+                a = u1;
+                u1 = a + alphaOne * (b - a);
+                u2 = a + alpha * (b - a);
+                functionU1 = function(u1);
+                functionU2 = function(u2);
+            } else if (functionU1 > functionU2) {
+                a = u1;
+                u1 = u2;
+                functionU1 = functionU2;
+                u2 = a + alpha * (b - a);
+                functionU2 = function(u2);
             } else {
-                b = right;
+                b = u2;
+                u2 = u1;
+                functionU2 = functionU1;
+                u1 = a + alphaOne * (b - a);
+                functionU1 = function(u1);
             }
         }
 
+        i++;
         System.out.println("Golden section: ");
         System.out.println("Point: " + ((b + a) / 2) + " Function: " + function((b + a) / 2));
+        System.out.println("Number of iterations: " + i);
     }
 
-    public static double parabola() {
-        return 2.0;
+    public static void parabola(double u1, double u3, double eps) {
+        double u2 = (u3 + u1) / 2;
+        int i = 0;
+        double u2Check = u2;
+
+        double deltaPlus = function(u1) - function(u2);
+        double deltaMinus = function(u3) - function(u2);
+
+        double w = (((Math.pow((u3 - u2), 2)) * deltaMinus) - ((Math.pow((u2 - u1), 2)) * deltaPlus))
+                / (2 * ((u3 - u2) * deltaMinus + (u2 - u1) * deltaPlus));
+
+        while (Math.abs(u2Check - w) >= eps) {
+            i++;
+
+            deltaPlus = function(u1) - function(u2);
+            deltaMinus = function(u3) - function(u2);
+
+            w = (((Math.pow((u3 - u2), 2)) * deltaMinus) - ((Math.pow((u2 - u1), 2)) * deltaPlus))
+                    / (2 * ((u3 - u2) * deltaMinus + (u2 - u1) * deltaPlus));
+
+            u2Check = u2;
+
+            if (w < u2) {
+                if (function(w) < function(u2)) {
+                    u3 = u2;
+                    u2 = w;
+                } else if (function(w) > function(u2)) {
+                    u1 = w;
+                } else {
+                    if (function(u1) > function(u2)) {
+                        u3 = u2;
+                        u2 = w;
+                    } else if (function(u2) > function(u3)) {
+                        u1 = w;
+                    }
+                }
+            } else if (w > u2) {
+                if (function(w) < function(u2)) {
+                    u1 = u2;
+                    u2 = w;
+                } else if (function(w) > function(u2)) {
+                    u3 = w;
+                } else {
+                    if (function(u3) > function(u2)) {
+                        u1 = u2;
+                        u2 = w;
+                    } else if (function(u1) > function(u2)) {
+                        u3 = w;
+                    }
+                }
+            } else {
+                if (function(u2 - 0.01) < function(u2)) {
+                    u2 -= 0.01;
+                } else {
+                    u2 += 0.01;
+                }
+            }
+        }
+
+        System.out.println("x: " + u2);
+        System.out.println("f(x): " + function(u2));
+        System.out.println("Number of iterations: " + i);
     }
 }
